57a41a5a3c9bf442a9d359b3726b160e27670eb5
Muhammad Arief <64636917+itsmerifz@users.noreply.github.com>
2021-07-28 18:36:34 +0800

    migrating to react

--- src/App.js
+++ src/App.js
@@ -2,9 +2,36 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="container">
+      <div className="frame">
+          <section className="header">
+              <button className="header-btn hitam">Add</button>
+              <h1 className="header-title">Todo Lists</h1>
+              <button className="header-btn merah">Clear</button>
+          </section>
+          
+          <section className="add">
+            <form className="add-form">
+              <input type="text" className="add-input"></input>
+              <button className="add-btn hitam">Add</button>
+            </form>
+          </section>
+
+          <section className="todos">
+            <div className="todo">
+              <span className="todo-txt">ss</span>
+            </div>
+            <div className="todo">
+              <span className="todo-txt">ss</span>
+            </div>
+            <div className="todo">
+              <span className="todo-txt">ss</span>
+            </div>
+            <div className="todo">
+              <span className="todo-txt">ss</span>
+            </div>
+          </section>
+      </div>
     </div>
   );
 }
